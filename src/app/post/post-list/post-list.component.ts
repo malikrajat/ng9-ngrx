24347ecdec8429038e7b2e171b8f5a75fc97e4c9
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-08 08:36:55 +0530

    update listing from store

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from '../store/post.model';
+import { Store, select } from '@ngrx/store';
+import { PostState } from '../store/post.reducer';
+import * as fromSelecter from '../store/post.selectors';
+import * as fromAction from '../store/post.actions';
 
 @Component({
   selector: 'app-post-list',
@@ -8,11 +12,27 @@ import { Post } from '../store/post.model';
   changeDetection: ChangeDetectionStrategy.Default,
 })
 export class PostListComponent implements OnInit {
-  posts$: Observable<Post>;
-  constructor() {}
+  posts$: Observable<Post[]>;
+  constructor(private _store: Store<PostState>) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._loadPosts();
+  }
+
+  private _loadPosts(): void {
+    this._store.pipe(select(fromSelecter.hasLoaded)).subscribe((res) => {
+      if (res) {
+        this.posts$ = this._store.pipe(select(fromSelecter.loadPostsList));
+      } else {
+        this._store.dispatch(fromAction.loadPosts());
+      }
+    });
+  }
   /**
    * deletePost: void  */
-  public deletePost(id: string): void {}
+  public deletePost(id: string): void {
+    if (window.confirm('Are sure you want to delete this item ?')) {
+      this._store.dispatch(fromAction.deletePost({ id }));
+    }
+  }
 }
