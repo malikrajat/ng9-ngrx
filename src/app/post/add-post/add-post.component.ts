6936f11d37bfef19fdc2b1e1c7149eb4827b9235
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-08 08:36:38 +0530

    add post component

--- src/app/post/add-post/add-post.component.ts
+++ src/app/post/add-post/add-post.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PostState } from '../store/post.reducer';
+import { Store } from '@ngrx/store';
+import { addPost } from '../store/post.actions';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styles: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class AddPostComponent implements OnInit {
+  addPost: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private _store: Store<PostState>) {}
 
   ngOnInit(): void {
+    this.addPost = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(6)]],
+      body: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  /**
+   * addNewPost: void  */
+  public addNewPost(): void {
+    this.submitted = true;
+    if (this.addPost.invalid) {
+      return;
+    }
+    this._store.dispatch(addPost({ post: this.addPost.value }));
+  }
+  get f() {
+    return this.addPost.controls;
+  }
 }
