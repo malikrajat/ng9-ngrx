3b787cfe4b47356ab2817e64c2e4913b0f955233
er.rajatmalik <er.rajatmalik@gmail.com>
2020-06-08 08:36:12 +0530

    add reactive

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PostRoutingModule } from './post-routing.module';
 import { AddPostComponent } from './add-post/add-post.component';
@@ -9,14 +10,18 @@ import { StoreModule } from '@ngrx/store';
 import * as fromPost from './store/post.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { PostEffects } from './store/post.effects';
+import { PostService } from './post.service';
 
 @NgModule({
   declarations: [AddPostComponent, EditPostComponent, PostListComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     PostRoutingModule,
     StoreModule.forFeature(fromPost.postsFeatureKey, fromPost.reducer),
     EffectsModule.forFeature([PostEffects]),
   ],
+  providers: [PostService],
 })
 export class PostModule {}
